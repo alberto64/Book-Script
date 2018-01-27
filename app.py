94ee909587cb7993173ba2d1d0899d3556320da9
bravopr <gustavo.bravo@upr.edu>
2018-01-26 20:30:04 -0400

    second commit

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from language import BookScript
 
 language = BookScript
-xmnvcbsdkbvkjv = 1
+
 print("Welcome to Book Script!!!! \n“A library at the run of a program”")
 while 1:
     language.run_code()
